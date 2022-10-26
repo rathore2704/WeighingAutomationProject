d9c21c437094014ed50e5378211bcdac5f655224
Deepak Singh Rathore <rathore270482@gmail.com>
2022-10-26 16:13:14 +0530

    newUpdate

--- prodTable.js
+++ prodTable.js
@@ -35,3 +35,30 @@ function updateDate() {
 }
 
 //"updateClock(); setInterval('updateClock()', 1000)"
+
+// AddRow fumction to add the weight entry and other parameters
+let sName = [];
+let ubin = [];
+let tankWeight = [];
+let time = [];
+
+let n = 1;
+let x = 0;
+
+function AddRow(){
+   let AddRow = document.getElementById("ptTable");
+   let NewRow = AddRow.insertRow(n);
+   //let newText = AddRow.in 
+
+   tankWeight[x] = document.getElementById("weightScreen").value;
+   time[x] = document.getElementById("clock").value;
+
+   let cellWt = NewRow.insertCell(0);
+   let cellTime = NewRow.insertCell(1);
+
+   cellWt.innerHTML = tankWeight[x];
+   cellTime.innerHTML = time[x];
+
+   n++;
+   x++;
+}
